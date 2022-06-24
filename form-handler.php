3e14ec14180cfeb4428dd35a55db4e0993127738
Nandini-Srinivas <99822833+Nandini-Srinivas@users.noreply.github.com>
2022-06-24 14:53:14 +0530

    Add files via upload

---
+++ form-handler.php
@@ -0,0 +1,28 @@
+<?php
+$name=$_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+
+$email_from = '2000031325@kluniversity.in';
+
+$email_subject='New Form Submission';
+
+$email_body="User Name: $name.\n".
+            "User Name: $visitor_email.\n".
+             "Subject: $subject.\n".
+             "User Message: $message.\n".
+
+
+$to = 'nandinipaleti7@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to, $email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+
+
+?>
